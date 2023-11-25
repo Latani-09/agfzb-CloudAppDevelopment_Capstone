ee964d5eaf3ab4eeed43effa28b60789faed7c30
latani-09 <latani2709@gmail.com>
2023-11-25 00:17:18 -0500

    super user create

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -31,16 +31,17 @@ def contact(request):
     return render(request, 'djangoapp/contactus.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    return render(request,'djangoapp/index.html')
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    return render(request,'djangoapp/index.html')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+
+    return render(request, 'djangoapp/registration.html')
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
